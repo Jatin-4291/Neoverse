8208179c27fd239ad2a9036950086eec75f17466
Jatin-4291 <jatin.dua2003@gmail.com>
2025-07-08 00:35:42 +0530

    commit-1 basic app structure done

---
+++ frontend/components/Navbar/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { createClient } from "@/utils/supabase/server";
+import { NavbarChild } from "./NavbarChild";
+import { formatEmailToName } from "@/utils/formatEmailToName";
+import { log } from "console";
+export const Navbar: React.FC = async () => {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  console.log("user", user);
+
+  return (
+    <NavbarChild
+      name={formatEmailToName(user?.user_metadata.email)}
+      avatar_url={user?.user_metadata.avatar_url}
+    />
+  );
+};
